7a72e1672a2d0470587de19bbaa82c29302b64dd
Connors <Brendan.Connors@dfo-mpo.gc.ca>
2024-09-06 06:46:39 -0700

    added in Winther et al. 2024 Skeena Chinook data

--- R/1_COSEWIC_PSE_compilation.R
+++ R/1_COSEWIC_PSE_compilation.R
@@ -17,15 +17,20 @@ skeena_file <- read.csv("data/dataset_1part1.May262022_Skeena.csv", header = T)
 hg_file <- read.csv("data/dataset_1part1.Jun82023_HG.csv", header = T)
 columbia_file <- read.csv("data/dataset_1part1.Feb122024_Columbia.csv", header = T)
 yukon_file <- read.csv("data/yukon_chinook_rr_escape.4Sep2024.csv", header = T)
+skeena2_file <- read.csv("data/Sep062024.Winther.Skeena.Chinook.csv", header = T)
 
 columbia_file <- read.csv("data/dataset_1part1.Feb122024_Columbia.csv", header = T) %>%
   #commas in these counts were messing things up
   mutate(LGL.counts = as.integer(gsub(",", "", LGL.counts)), 
          NuSEDS.counts.by.CU = as.integer(gsub(",", "", NuSEDS.counts.by.CU))) 
 
+# drop older Skeena chinook data for CUs with newer data
+skeena_file <- skeena_file %>%
+  filter(! CUID %in% c(217, 212, 222, 210, 229, 207))
+
 # combine files from each region, switched this to bind_rows to keep the functions in 
   #tidyverse-land. bind_rows has useful warnings.  
-cu_dat <- bind_rows(cc_file,fraser_file,vimi_file,nass_file,skeena_file,hg_file,columbia_file,yukon_file) %>%
+cu_dat <- bind_rows(cc_file,fraser_file,vimi_file,nass_file,skeena_file,hg_file,columbia_file,yukon_file,skeena2_file) %>%
   select(CUID, Species, Year, LGL.counts, Region)
 
 #check species names
